8dafc82d1a3265538baaab6a2bdfb685d734a89a
Luana Neumann <luanafneumann.22@gmail.com>
2024-07-11 23:21:22 -0300

    create passwordRoute

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,22 @@
+import { dataSource } from '@shared/infra/typeorm';
+import UserToken from '../entities/userToken';
+
+export const UserTokensRepository = dataSource.getRepository(UserToken).extend({
+  async findByToken(token: string): Promise<UserToken | null> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+    return userToken;
+  },
+
+  async generate(user_id: string): Promise<UserToken | null> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+    return userToken;
+  },
+});
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -7,6 +7,8 @@ import { CreateProducts1716916478931 } from './migrations/1716916478931-CreatePr
 import { CreateUsers1717011851779 } from './migrations/1717011851779-CreateUsers';
 import Product from '@modules/products/typeorm/entities/product';
 import User from '@modules/users/typeorm/entities/user';
+import { CreateUserTokens1720637978543 } from './migrations/1720637978543-CreateUserTokens';
+import UserToken from '@modules/users/typeorm/entities/userToken';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -15,6 +17,10 @@ export const dataSource = new DataSource({
   username: 'postgres',
   password: '181500',
   database: 'apivendas',
-  entities: [Product, User],
-  migrations: [CreateProducts1716916478931, CreateUsers1717011851779],
+  entities: [Product, User, UserToken],
+  migrations: [
+    CreateProducts1716916478931,
+    CreateUsers1717011851779,
+    CreateUserTokens1720637978543,
+  ],
 });
