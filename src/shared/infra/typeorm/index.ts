d272e275907083e5751f5d9bc857126efdfe9151
Luana Neumann <luanafneumann.22@gmail.com>
2024-07-25 17:19:53 -0300

    Creating entity Order

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -11,6 +11,11 @@ import { CreateUserTokens1720637978543 } from './migrations/1720637978543-Create
 import UserToken from '@modules/users/typeorm/entities/userToken';
 import { CreateCustomers1721679379653 } from './migrations/1721679379653-CreateCustomers';
 import Customer from '@modules/customers/typeorm/entities/Customer';
+import { CreateOrders1721936794355 } from './migrations/1721936794355-CreateOrders';
+import { AddCustomerIdToOrders1721937039360 } from './migrations/1721937039360-AddCustomerIdToOrders';
+import { CreateOrdersProducts1721937650192 } from './migrations/1721937650192-CreateOrdersProducts';
+import { AddOrderIdToOrdersProducts1721937918626 } from './migrations/1721937918626-AddOrderIdToOrdersProducts';
+import { AddProductIdToOrdersProducts1721938229646 } from './migrations/1721938229646-AddProductIdToOrdersProducts';
 
 export const dataSource = new DataSource({
   type: 'postgres',
@@ -25,5 +30,10 @@ export const dataSource = new DataSource({
     CreateUsers1717011851779,
     CreateUserTokens1720637978543,
     CreateCustomers1721679379653,
+    CreateOrders1721936794355,
+    AddCustomerIdToOrders1721937039360,
+    CreateOrdersProducts1721937650192,
+    AddOrderIdToOrdersProducts1721937918626,
+    AddProductIdToOrdersProducts1721938229646,
   ],
 });
