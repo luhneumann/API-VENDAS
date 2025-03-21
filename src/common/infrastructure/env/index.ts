3e8a52e4784a23a79ec1e67a23925326d8c516db
Luana Neumann <luanafneumann.22@gmail.com>
2025-03-21 11:08:29 -0300

    configuração typeorm e conexão com bd via docker compose

--- src/common/infrastructure/env/index.ts
+++ src/common/infrastructure/env/index.ts
@@ -6,7 +6,14 @@ import {z} from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   PORT: z.coerce.number().default(3333), //Todos os valores dentro do .env são considerados strings. Para que essa validação como number não gere erro, adicionamos o coerce e analisa a string e verifica se o valor dentro da string é numérico ou não.
-  API_URL: z.string().default('http://localhost:3333')
+  API_URL: z.string().default('http://localhost:3333'),
+  DB_TYPE: z.literal('postgres').default('postgres'),
+  DB_HOST: z.string().default('localhost'),
+  DB_PORT: z.coerce.number().default(54322),
+  DB_SCHEMA: z.string().default('public'),
+  DB_NAME: z.string().default('postgres'),
+  DB_USER: z.string().default('postgres'),
+  DB_PASS: z.string().default('postgres')
 })
 
 const _env = envSchema.safeParse(process.env)
--- src/common/infrastructure/http/server.ts
+++ src/common/infrastructure/http/server.ts
@@ -1,9 +1,16 @@
 import { env } from "../env";
+import { dataSource } from "../typeorm";
 import { app } from "./app";
 
+dataSource
+  .initialize()
+  .then(() => {
+    app.listen(env.PORT, () => {
+      console.log(`Server is running on port ${env.PORT}`)
+      console.log('API docs available at GET /docs')
+    })
+  }).catch((error)=> {
+    console.error('Error initializing data source', error)
+  })
 
 
-app.listen(env.PORT, () => {
-  console.log(`Server is running on port ${env.PORT}`)
-  console.log('API docs available at GET /docs')
-})
